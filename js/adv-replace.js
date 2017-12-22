00e4ebc425da3f2fcb4d5b305d48f07939fc62be
jekyll <jekyll@gmail.com>
2017-12-22 18:32:45 +0530

    few changes ad

--- js/adv-replace.js
+++ js/adv-replace.js
@@ -1,2 +1,20 @@
+/* 
 window.onload=function(){setTimeout(function(){var a=document.querySelector(".adsbygoogle");a&&0==a.innerHTML.replace(/\s/g,"").length&&(a.style.cssText="display:block !important; visibility:visible !important;text-decoration:none !important;-moz-binding:none !important",a.innerHTML="<a href='http://webjeda.com'><div class='replace'>Get a beautiful website designed by us. </div></a>");(a=document.querySelector(".adclass"))&&0==a.innerHTML.replace(/\s/g,"").length&&(a.style.cssText="display:block !important; visibility:visible !important;text-decoration:none !important;-moz-binding:none !important",
 a.innerHTML="<a href='https://jekyll-themes.com'><div class='replace'> Find the latest Jekyll Themes </div></a>");(a=document.querySelector(".adclass2"))&&0==a.innerHTML.replace(/\s/g,"").length&&(a.style.cssText="display:block !important; visibility:visible !important;text-decoration:none !important;-moz-binding:none !important",a.innerHTML="<a href='http://webjeda.com'><div class='replace'> Looking for a Jekyll website? Log in to Webjeda </div></a>")},2E3)};
+
+
+
+
+window.onload=function(){setTimeout(function(){
+         var c="display:block !important; visibility:visible !important;text-decoration:none !important;-moz-binding:none !important";
+         var b= "<div class='replace'><a target='_blank' href='http://blog.webjeda.com'><svg version='1.1' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' x='0px' y='0px' width='300px' height='296px' viewBox='0 0 300 240' enable-background='new 0 0 300 296'><text transform='matrix(1 0 0 1 35.1719 102)'><tspan x='0' y='0' fill='#555' font-family='Lato' font-size='20'>Looking for Jekyll Tutorials?</tspan><tspan x='60' y='32' fill='#555' font-family='Lato'  font-size='20'>Click here </tspan></text><g><path fill='#0a5' d='M216.747,115.899v7.439H199.4v8.924h17.347v7.437l17.347-11.899L216.747,115.899z'/></g></svg></div>";
+         var d= "<div class='default-replace'><a href='https://avocado.jekyll-themes.com'> Check out Avocado Jekyll Theme! </a></div>";
+         var a=document.querySelector("ins.adsbygoogle");
+         a&&0==a.innerHTML.replace(/\s/g,"ins.adsbygoogle.adclass").length&&(a.style.cssText=c,a.innerHTML=b);
+         var a=document.querySelector("ins..adsbygoogle.adclass2");
+         a&&0==a.innerHTML.replace(/\s/g,"").length&&(a.style.cssText=c,a.innerHTML=d);
+     },
+800)};
+
+
+*/
